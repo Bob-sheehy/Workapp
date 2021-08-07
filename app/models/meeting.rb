c568468db234925c8790f48e889ea9b4214a48c0
Bob-sheehy <bob_sheehy@hotmail.com>
2021-08-07 19:18:42 +0100

    third Commit

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -12,7 +12,19 @@ class Meeting < ApplicationRecord
    #self.duration = (end_date - start_date).to_i
     
     #end
-   
+    def daily_total_duration
+      # Meeting.where("start_date >= ? AND end_date < ?", Time.now.beginning_of_day, Time.now.end_of_day).sum(:duration)
+       #@meeting = Meeting.where("start_date >= ?", Time.now.beginning_of_week).sum(:duration)
+    #  @meeting =  Meeting.where("start_date >= ? AND end_date < ?", Time.now.beginning_of_day, Time.now.end_of_day).sum(:duration)
+
+      Meeting.where("start_date >= ? AND end_date  < ?", Time.now.beginning_of_day, Time.now.end_of_day).sum(:duration)
+      end
+
+      def weekly_total_duration
+        #Meeting.where("start_date >= ? AND end_date < ?", Time.now.beginning_of_day, Time.now.end_of_day).sum(:duration)
+        Meeting.where("start_date >= ?", Time.now.beginning_of_week).sum(:duration)
+      end 
+      
    # def all_day
     #  Meeting.where("created_at >= ? AND created_at < ?", Time.now.beginning_of_day, Time.now.end_of_day).sum(:duration)
 
